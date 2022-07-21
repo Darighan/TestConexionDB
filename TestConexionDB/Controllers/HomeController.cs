6155761684d893d6783ae5b020afc5a005ca3c8b
Felipe Reichert <darighan@github.com>
2022-07-21 17:21:45 -0400

    Agregar archivos de proyecto.

---
+++ TestConexionDB/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestConexionDB.Models;
+
+namespace TestConexionDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult TestDB()
+        {
+            var db = new ClienteContext().Cliente;
+            var clientetest = db.Cliente.tolist();
+
+            ViewBag.Cliente = clientetest;
+
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TestConexionDB/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore;
+namespace TestConexionDB.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string nombreCliente { get; set; }
+
+    }
+
+    public class ClienteContext : DbContext 
+    {
+        public DbSet<Cliente> Cliente { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseMySQL("server=localhost;database=testcanchas;user=root;password=");
+        }
+
+
+    }
+
+
+}
